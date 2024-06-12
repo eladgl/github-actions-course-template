a55f9cda40bcfc5c0d7d57ce8e47ddebfaec548c
Elad Goldenberg <59554824+eladgl@users.noreply.github.com>
2024-06-12 12:41:35 +0300

    Update game.test.js

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -2,11 +2,14 @@ const Game = require('../src/game').default
 const fs = require('fs')
 
 describe('App', () => {
-  it('Contains the compiled JavaScript', async () => {
-    const data = await readFile('./public/main.js', 'utf8');
-    expect(data).toMatchSnapshot();
-  });
-});
+  it('Contains the compiled JavaScript', async (done) => {
+    fs.readFile('./public/main.js', 'utf8', (err, data) => {
+      expect(err).toBe(null)
+      expect(data).toMatchSnapshot()
+      done()
+    })
+  })
+})
 
 describe('Game', () => {
   let game, p1, p2
